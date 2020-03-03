5d116ecf39670207a04a031ba65804dfefc380ec
Nagi909 <papelusa43@gmail.com>
2020-03-03 10:24:37 +0100

    "Creating volunteer time"

---
+++ src/main/java/model/VolunteerTime.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class VolunteerTime {
+    private Date date;
+    public VolunteerTime(){}
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
