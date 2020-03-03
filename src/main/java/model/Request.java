5f90d6d6f4a877d6557d3476d51ce0126f508fb4
Nagi909 <papelusa43@gmail.com>
2020-03-03 09:32:12 +0100

    "Request"

--- src/main/java/model/Request.java
+++ src/main/java/model/Request.java
@@ -6,5 +6,5 @@ public class Request {
     private String id;
     private String TypeOfService;
     private Date schedule;
-
+    private RequestState requestState;
 }
---
+++ src/main/java/model/RequestState.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum RequestState {
+    approved,
+    rejected,
+    waiting
+}
